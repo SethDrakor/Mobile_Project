b0e43d1b374017a7beaea85a642c1e112638b542
SethDrakor <seth.drakor@gmail.com>
2025-03-09 18:41:50 +0100

    SetupScene

---
+++ Assets/Project/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private float moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+    }
+}
